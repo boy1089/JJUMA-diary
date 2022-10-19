85f2916769a713f94eaf53b7a81b2a59cf378df8
boy1089 <boytoboy0108@gmail.com>
2022-10-20 00:37:53 +0900

    in parsing googlePhotoResponse, creationTime is used to get timestamp of image, not filename.

--- lib/Util/responseParser.dart
+++ lib/Util/responseParser.dart
@@ -15,22 +15,29 @@ List<List<String>> parseResponse(response) {
   List<String> datetimes = ["time"];
 
   for (int i = 0; i < responseToString.length; i++) {
+    if (responseToString[i].contains('creationTime')){
+      String time = responseToString[i].split("{").last;
+      time = time.split(' ').last.replaceAll("-", '').replaceAll("T", '_').replaceAll(':', '').substring(0, 15);
+      print("time : $time");
+      datetimes.add(time);
+    }
+
     if (responseToString[i].contains("https://lh3.googleusercontent.com/")) {
       links.add(responseToString[i].substring(10));
     }
-    if (responseToString[i].contains("filename")) {
-      filenames.add(responseToString[i].substring(11).split('}').first);
-      String filename = responseToString[i].substring(11).split('}').first;
-      filenames.add(filename);
-
-      try {
-        datetimes.add(filename.substring(0, 15));
-      } catch(e){
-        datetimes.add(filename);
-      }
-
-
-    }
+    //
+    // if (responseToString[i].contains("filename")) {
+    //   filenames.add(responseToString[i].substring(11).split('}').first);
+    //   String filename = responseToString[i].substring(11).split('}').first;
+    //   filenames.add(filename);
+    //
+    //   try {
+    //     datetimes.add(filename.substring(0, 15));
+    //   } catch(e){
+    //     datetimes.add(filename);
+    //   }
+    //   print(filename.substring(0, 15));
+    // }
   }
   List<List<String>> result = [datetimes, links];
   // result.insert(0, ["time", "link", "filename"]);

2c455107b881eef970ae8838b02845f73985ecf9
boy1089 <boytoboy0108@gmail.com>
2022-11-22 01:10:34 +0900

    update PermissionPAge page is wrapped with scaffold.
    shakeWidget is now also wrap text to emphasize what to do.

--- lib/pages/PermissionPage.dart
+++ lib/pages/PermissionPage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lateDiary/Permissions/PermissionManager.dart';
+import 'package:lateDiary/Util/Util.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:lateDiary/Util/global.dart' as global;
 import 'package:flutter_shake_animated/flutter_shake_animated.dart';
-
 
 class PermissionPage extends StatefulWidget {
   late PermissionManager permissionManager;
@@ -31,69 +31,77 @@ class _PermissionPageState extends State<PermissionPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+            color: Colors.white,
+            width: physicalWidth * 2 / 3,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Offstage(
+                    offstage:
+                        widget.permissionManager.isStoragePermissionGranted,
+                    child: CustomShakeWidget(
+                            Text(
+                              "Access to photo is needed to display photo in the device.",
+                              style: TextStyle(
+                                  fontSize: 20, color: global.kColor_diaryText),
+                            ),
+                            shakeButton)
+                        .build(context),
+                  ),
+                  SizedBox(height: 50),
+                  Offstage(
+                    offstage:
+                        widget.permissionManager.isLocationPermissionGranted,
+                    child: CustomShakeWidget(
+                            Text(
+                                "Access to location is needed to analyze the meta data of photo.",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: global.kColor_diaryText)),
+                            shakeButton)
+                        .build(context),
+                  ),
+                  SizedBox(height: 50),
+                  ElevatedButton(
+                      onPressed: () async {
+                        await openAppSettings();
+                        await onClicked();
+                        setState(() {});
+                      },
+                      child: const Text("Allow permission")),
+                  ShakeWidget(
+                    duration: Duration(milliseconds: global.animationTime),
+                    shakeConstant: ShakeHorizontalConstant1(),
+                    autoPlay: shakeButton,
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          await checkOkToProceed();
+                          if (isOkToProceed) {
+                            Navigator.pop(context, true);
+                            // Navigation.navigateTo(
+                            //     context: context,
+                            //     screen: MyApp(),
+                            //     style: NavigationRouteStyle.material);
+                          }
 
-
-              Offstage(
-                offstage:
-                    widget.permissionManager.isStoragePermissionGranted,
-                child: const Text(
-                  "Access to photo is needed to display photo in the device.",
-                  style: const TextStyle(
-                      fontSize: 20, color: global.kColor_backgroundText),
-                ),
-              ),
-
-              SizedBox(height: 50),
-
-              Offstage(
-                offstage: widget.permissionManager.isLocationPermissionGranted,
-                child: const Text(
-                    "Access to location is needed to analyze the meta data of photo.",
-                    style: TextStyle(
-                        fontSize: 20, color: global.kColor_backgroundText)),
-              ),
-              SizedBox(height: 50),
-
-              ElevatedButton(
-                  onPressed: () async {
-                    await openAppSettings();
-                    await onClicked();
-                    setState(() {});
-                  },
-                  child: const Text("Allow permission")),
-
-              ShakeWidget(
-                duration: Duration(milliseconds: global.animationTime),
-                shakeConstant: ShakeHorizontalConstant1(),
-                autoPlay: shakeButton,
-                child: ElevatedButton(
-                    onPressed: () async {
-                      await checkOkToProceed();
-                      if (isOkToProceed) {
-                        Navigator.pop(context, true);
-                        // Navigation.navigateTo(
-                        //     context: context,
-                        //     screen: MyApp(),
-                        //     style: NavigationRouteStyle.material);
-                      }
-
-                      setState(() {
-                        shakeButton = true;
-                      });
-                      await Future.delayed(Duration(milliseconds: 300));
-                      setState(() {
-                        shakeButton = false;
-                      });
-                    },
-                    child: Text("continue")),
-              ),
-            ]));
+                          setState(() {
+                            shakeButton = true;
+                          });
+                          await Future.delayed(Duration(milliseconds: 300));
+                          setState(() {
+                            shakeButton = false;
+                          });
+                        },
+                        child: Text("continue")),
+                  ),
+                ])),
+      ),
+    );
   }
 
   Future<void> onClicked() async {
@@ -105,5 +113,21 @@ class _PermissionPageState extends State<PermissionPage> {
     await widget.permissionManager.checkLocationPermission();
     await widget.permissionManager.checkMediaLibraryPermission();
     return;
+  }
+}
+
+class CustomShakeWidget {
+  Widget widget;
+  bool shakeButton;
+  CustomShakeWidget(this.widget, this.shakeButton);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShakeWidget(
+      duration: Duration(milliseconds: global.animationTime),
+      shakeConstant: ShakeHorizontalConstant1(),
+      autoPlay: shakeButton,
+      child: widget,
+    );
   }
 }

9a49629def031f181b027db3ed69fe1f7a80a644
boy1089 <boytoboy0108@gmail.com>
2022-11-29 11:06:36 +0900

    backbutton is implemented in leftTop side of screen.

--- lib/pages/MainPage/MainPageViewIos.dart
+++ lib/pages/MainPage/MainPageViewIos.dart
@@ -30,65 +30,32 @@ class _MainPageViewIosState extends State<MainPageViewIos> {
     dayPageStateProvider =
         Provider.of<DayPageStateProvider>(context, listen: false);
     yearPageStateProvider =
-        Provider.of<YearPageStateProvider>(context, listen: false);
-
-    return  CupertinoTabScaffold(
+        Provider.of<YearPageStateProvider>(context, listen: true);
+    bool offstage = offstageLogicForBackbutton();
+    return Scaffold(
+      floatingActionButton: Offstage(
+        offstage: offstage,
+        child: IconButton(
+          icon: Icon(CupertinoIcons.arrow_left),
+          onPressed: () => onBackButton(),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartTop,
+      body: CupertinoTabScaffold(
           tabBar: CupertinoTabBar(
-            height : global.kBottomNavigationBarHeight,
+            height: global.kBottomNavigationBarHeight,
             items: [
               BottomNavigationBarItem(
                   icon: Icon(Icons.photo_camera_back_outlined)),
               BottomNavigationBarItem(icon: Icon(Icons.bookmark)),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.settings)),
+              BottomNavigationBarItem(icon: Icon(Icons.settings)),
             ],
           ),
           tabBuilder: (context, index) {
             if (index == 2) index = 3;
             return widget._widgetOptions.elementAt(index);
-          });
-
-    // return Scaffold(
-    //   body: PageTransitionSwitcher(
-    //     duration: const Duration(milliseconds: 1000),
-    //     transitionBuilder: (child, primaryAnimation, secondaryAnimation) =>
-    //         FadeThroughTransition(
-    //           animation: primaryAnimation,
-    //           secondaryAnimation: secondaryAnimation,
-    //           child: child,
-    //         ),
-    //     child:
-    //     widget._widgetOptions[navigationProvider.currentNavigationIndex.index],
-    //   ),
-    //   backgroundColor: global.kBackGroundColor,
-    //
-    //
-    //   bottomNavigationBar: Container(
-    //     height: global.kBottomNavigationBarHeight,
-    //     // width : 200,
-    //     color: Colors.red,
-    //     child: Offstage(
-    //       offstage: !navigationProvider.isBottomNavigationBarShown,
-    //       child: BottomNavigationBar(
-    //         selectedFontSize: 0,
-    //         type: BottomNavigationBarType.fixed,
-    //         items: const <BottomNavigationBarItem>[
-    //           BottomNavigationBarItem(
-    //               icon: Icon(Icons.photo_camera_back_outlined),
-    //               label: "Photo"),
-    //           BottomNavigationBarItem(
-    //               icon: Icon(Icons.bookmark), label: "Diary"),
-    //           BottomNavigationBarItem(
-    //               icon: Icon(Icons.settings), label: "Settings"),
-    //         ],
-    //         currentIndex: navigationProvider.currentNavigationIndex.index,
-    //         onTap: (index) {
-    //           onTap(context, navigationIndex.values[index]);
-    //         },
-    //       ),
-    //     ),
-    //   ),
-    // );
+          }),
+    );
   }
 
   void onTap(BuildContext context, navigationIndex item) {
@@ -108,6 +75,53 @@ class _MainPageViewIosState extends State<MainPageViewIos> {
             screen: AndroidSettingsScreen(),
             style: NavigationRouteStyle.material);
     }
+  }
+
+  void onBackButton() {
+    switch (navigationProvider.currentNavigationIndex) {
+      case navigationIndex.year:
+        if (yearPageStateProvider.isZoomIn) {
+          setState(() {
+            yearPageStateProvider.setZoomInState(false);
+            yearPageStateProvider.setZoomInRotationAngle(0);
+          });
+        }
+        break;
+      case navigationIndex.diary:
+        navigationProvider.setNavigationIndex(navigationIndex.year);
+        break;
+      case navigationIndex.day:
+        //when zoomed in, make daypage zoom out
+        global.indexForZoomInImage = -1;
+        global.isImageClicked = false;
+
+        if (dayPageStateProvider.isZoomIn) {
+          setState(() {
+            dayPageStateProvider.setZoomInState(false);
+            dayPageStateProvider.setZoomInRotationAngle(0);
+          });
+        }
+
+        if (navigationProvider.lastNavigationIndex == navigationIndex.diary) {
+          navigationProvider
+              .setNavigationIndex(navigationProvider.lastNavigationIndex);
+          break;
+        }
+        //when zoomed out, go to month page
+        if (!dayPageStateProvider.isZoomIn) {
+          navigationProvider.setNavigationIndex(navigationIndex.year);
+        }
+        break;
+    }
+  }
+
+  bool offstageLogicForBackbutton() {
+    print(yearPageStateProvider.isZoomIn);
+    if (navigationProvider.isZoomIn) return false;
+    if (yearPageStateProvider.isZoomIn) return false;
+    if (navigationProvider.currentNavigationIndex == navigationIndex.day)
+      return false;
+    return true;
   }
 }
 

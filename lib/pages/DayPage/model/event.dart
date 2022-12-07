e0604de1d5640c12432a4db7805911044c363db7
boy1089 <boytoboy0108@gmail.com>
2022-12-07 09:22:44 +0900

    event class is implemented

---
+++ lib/pages/DayPage/model/event.dart
@@ -0,0 +1,23 @@
+import 'package:lateDiary/Data/infoFromFile.dart';
+
+
+class Event{
+
+  String? id = null;
+  Map<dynamic, InfoFromFile> images;
+
+  Event({required this.images, this.id});
+
+  factory Event.fromId({required id}){
+    //read file
+    //parse, return
+    Map<dynamic, InfoFromFile> images = {};
+    return Event(images : images);
+  }
+
+  factory Event.fromImages({required images}){
+    return Event(images : images);
+  }
+
+
+}

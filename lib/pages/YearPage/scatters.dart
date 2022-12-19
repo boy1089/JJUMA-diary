b85c6587745b02822734bfa77502f39eef1b1281
boy1089 <boytoboy0108@gmail.com>
2022-12-20 00:10:31 +0900

    Default CircleScatter and ImageScatter is implemented in scatters.dart

--- lib/pages/YearPage/scatters.dart
+++ lib/pages/YearPage/scatters.dart
@@ -1,7 +1,8 @@
 
+import 'dart:io';
+
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-
-
 
 
 class DefaultRectangleScatter extends StatelessWidget {
@@ -14,5 +15,34 @@ class DefaultRectangleScatter extends StatelessWidget {
     return Container(width: size, height: size,
       decoration:
       ShapeDecoration(shape: const Border(), color: color));
+  }
+}
+
+class DefaultCircleScatter extends StatelessWidget {
+  double size;
+  Color color;
+  DefaultCircleScatter({Key? key, required this.size, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: size, height: size,
+        decoration:
+        ShapeDecoration(shape: const CircleBorder(), color: color));
+  }
+}
+
+
+class ImageScatter extends StatelessWidget {
+  double size;
+  Color color;
+  String imagePath;
+  ImageScatter({Key? key, required this.size, required this.color, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: size, height: size,
+        decoration:
+        ShapeDecoration(shape: const Border(), color: color),
+    child : ExtendedImage.file(File(imagePath)));
   }
 }
